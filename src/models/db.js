1958dd2f1509923364f8910be2dc694965dd7641
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2024-03-29 17:57:37 +0000

    First commit - Playlists became Categories

---
+++ src/models/db.js
@@ -0,0 +1,12 @@
+import { userMemStore } from "./mem/user-mem-store.js";
+import { categoryMemStore } from "./mem/category-mem-store.js";
+
+export const db = {
+  userStore: null,
+  categoryStore: null,
+
+  init() {
+    this.userStore = userMemStore;
+    this.categoryStore = categoryMemStore;
+  },
+};
