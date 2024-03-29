21eb9a5f9c58c8bc9c9daaabc683cf12e92e1607
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2024-03-29 23:46:38 +0000

    Introduce JSON data store

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,10 @@
-import { userMemStore } from "./mem/user-mem-store.js";
-import { categoryMemStore } from "./mem/category-mem-store.js";
-import { schoolMemStore } from "./mem/school-mem-store.js";
+// import { userMemStore } from "./mem/user-mem-store.js";
+// import { categoryMemStore } from "./mem/category-mem-store.js";
+// import { schoolMemStore } from "./mem/school-mem-store.js";
+
+import { userJsonStore } from "./json/user-json-store.js";
+import { categoryJsonStore } from "./json/category-json-store.js";
+import { schoolJsonStore } from "./json/school-json-store.js";
 
 export const db = {
   userStore: null,
@@ -8,8 +12,8 @@ export const db = {
   schoolStore: null,
 
   init() {
-    this.userStore = userMemStore;
-    this.categoryStore = categoryMemStore;
-    this.schoolStore = schoolMemStore;
+    this.userStore = userJsonStore;
+    this.categoryStore = categoryJsonStore;
+    this.schoolStore = schoolJsonStore;
   },
 };
---
+++ src/models/json/school-json-store.js
@@ -0,0 +1,47 @@
+import { v4 } from "uuid";
+import { db } from "./store-utils.js";
+
+export const schoolJsonStore = {
+  async getAllSchools() {
+    await db.read();
+    return db.data.schools;
+  },
+
+  async addSchool(categoryId, school) {
+    await db.read();
+    school._id = v4();
+    school.categoryid = categoryId;
+    db.data.schools.push(school);
+    await db.write();
+    return school;
+  },
+
+  async getSchoolsByCategoryId(id) {
+    await db.read();
+    return db.data.schools.filter((school) => school.categoryid === id);
+  },
+
+  async getSchoolById(id) {
+    await db.read();
+    return db.data.schools.find((school) => school._id === id);
+  },
+
+  async deleteSchool(id) {
+    await db.read();
+    const index = db.data.schools.findIndex((school) => school._id === id);
+    db.data.schools.splice(index, 1);
+    await db.write();
+  },
+
+  async deleteAllSchools() {
+    db.data.schools = [];
+    await db.write();
+  },
+
+  async updateSchool(school, updatedSchool) {
+    school.schoolname = updatedSchool.schoolname;
+    school.language = updatedSchool.language;
+    school.location = updatedSchool.location;
+    await db.write();
+  },
+};
