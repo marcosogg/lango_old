c208c671e8c68131a3fa0148b6795a8cfc327209
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2024-03-30 16:06:05 +0000

    TDD implemented

--- src/models/db.js
+++ src/models/db.js
@@ -1,19 +1,26 @@
-// import { userMemStore } from "./mem/user-mem-store.js";
-// import { categoryMemStore } from "./mem/category-mem-store.js";
-// import { schoolMemStore } from "./mem/school-mem-store.js";
-
+import { userMemStore } from "./mem/user-mem-store.js";
+import { categoryMemStore as categoryMemStore } from "./mem/category-mem-store.js";
+import { schoolMemStore as schoolMemStore } from "./mem/school-mem-store.js";
 import { userJsonStore } from "./json/user-json-store.js";
-import { categoryJsonStore } from "./json/category-json-store.js";
-import { schoolJsonStore } from "./json/school-json-store.js";
+import { categoryJsonStore as categoryJsonStore } from "./json/category-json-store.js";
+import { schoolJsonStore as schoolJsonStore } from "./json/school-json-store.js";
 
 export const db = {
   userStore: null,
   categoryStore: null,
   schoolStore: null,
 
-  init() {
-    this.userStore = userJsonStore;
-    this.categoryStore = categoryJsonStore;
-    this.schoolStore = schoolJsonStore;
+  init(storeType) {
+    switch (storeType) {
+      case "json":
+        this.userStore = userJsonStore;
+        this.categoryStore = categoryJsonStore;
+        this.schoolStore = schoolJsonStore;
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.categoryStore = categoryMemStore;
+        this.schoolStore = schoolMemStore;
+    }
   },
 };
